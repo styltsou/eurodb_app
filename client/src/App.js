1fe20a73b893b0c3fad9d78e60d3810496ac945c
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-07 19:17:20 +0200

    Configured nginx container as reverse-proxy. WebSocket connection fail error

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     const testAPI = async () => {
-      const res = await fetch('http://localhost:5000/api/countries/most-wins');
+      const res = await fetch('/api/countries/most-wins');
       const data = await res.json();
       console.log(data);
     };
