1554934fcf2f25dc2dff47d1fb11956465d67326
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-14 23:49:49 +0200

    Rwemoved something from react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import { useState, useEffect } from "react";
 
 function App() {
-	let genre = "Electronic";
-
-	const [tracks, setTracks] = useState("");
+	const [contest, setContest] = useState("");
 
 	useEffect(() => {
 		const testAPI = async () => {
 			const res = await fetch(`/api/contests/2014`);
 			const data = await res.json();
-			setTracks(data);
+			setContest(data);
 		};
 
 		testAPI();
@@ -19,7 +17,7 @@ function App() {
 		<>
 			<h1>EuroDB</h1>
 			<div>
-				<pre>{JSON.stringify(tracks, null, 2)}</pre>
+				<pre>{JSON.stringify(contest, null, 2)}</pre>
 			</div>
 		</>
 	);
